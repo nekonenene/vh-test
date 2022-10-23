42a015b38c39237af8ff38662fb9a86853dec30c
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2022-10-24 03:06:50 +0900

    縦スクロールができるように

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,11 @@ import styles from './index.module.scss';
 
 function IndexPage() {
   return (
-    <main>
-      <div className={styles.container}>
+    <main className={styles.main}>
+      <div id="modal" className={styles.modalContainer}>
         <div className={styles.card}>
           <div className={styles.cardContentContainer}>
-            <h1 className={styles.modalTitle}>ようこそ</h1>
+            <h3 className={styles.modalTitle}>ようこそ</h3>
             <button className={styles.bottomButton}>次に進む</button>
           </div>
         </div>
