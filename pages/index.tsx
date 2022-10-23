192a5263a18a87db4ae0b0ab11a1357efa8bff77
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2022-10-24 03:46:42 +0900

    モーダルの表示・非表示が切り替わるように

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
+import React from 'react';
 import styles from './index.module.scss';
 
 function IndexPage() {
+  const [isOpenModal, setIsOpenModal] = React.useState(false);
+
   return (
     <main className={styles.main}>
-      <div id="modal" className={styles.modalContainer}>
+      <button className={styles.openModalButton} onClick={() => setIsOpenModal(true)}>モーダル表示</button>
+      <div id="modal" className={`${styles.modal} ${isOpenModal ? '' : styles.modalHidden}`}>
         <div className={styles.card}>
           <div className={styles.cardContentContainer}>
             <h3 className={styles.modalTitle}>ようこそ</h3>
-            <button className={styles.bottomButton}>次に進む</button>
+            <button className={styles.bottomButton} onClick={() => setIsOpenModal(false)}>モーダル削除</button>
           </div>
         </div>
       </div>
